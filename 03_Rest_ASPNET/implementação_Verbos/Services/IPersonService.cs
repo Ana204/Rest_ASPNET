1c4dc3b2914abfbe1e836dd5022bfa3cd2d62ada
ana204 <anagomes.lucia@hotmail.com>
2021-04-01 20:14:03 -0300

    interface Person

---
+++ 03_Rest_ASPNET/implementação_Verbos/Services/IPersonService.cs
@@ -0,0 +1,19 @@
+using IMPLEMENTAÇÃO_VERBOS.Model;
+using System.Collections.Generic;
+
+namespace IMPLEMENTAÇÃO_VERBOS.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindByID(long id);
+
+        List<Person> FindALL();
+
+        Person Update(Person person);
+        
+        void Delete(long id);
+
+    }
+}
