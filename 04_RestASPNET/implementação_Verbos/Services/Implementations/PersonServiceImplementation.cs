f975d824d8363f8998bd653404438865d8e6ad5b
ana204 <anagomes.lucia@hotmail.com>
2021-04-10 23:19:07 -0300

    personServiceImplementation finish

--- 04_RestASPNET/implementação_Verbos/Services/Implementations/PersonServiceImplementation.cs
+++ 04_RestASPNET/implementação_Verbos/Services/Implementations/PersonServiceImplementation.cs
@@ -16,6 +16,20 @@ namespace implementação_Verbos.Services.Implementations
         {
             _context = context;
         }
+
+        //para listar todos as pessoas
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        //listar pelo id
+        public Person FindByID(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        //criação de uma nova pessoa
         public Person Create(Person person)
         {
             try
@@ -31,29 +45,55 @@ namespace implementação_Verbos.Services.Implementations
            return person;
         }
 
-        public void Delete(long id){}
-
-        public List<Person> FindAll()
-        {
-            return _context.Persons.ToList();
-        }
-
-        public Person FindByID(long id)
-        {
-           return new Person
-           {
-               Id = 1,
-               FirstName = "Ana",
-               LastName = "Gomes",
-               Address = "São Paulo - SP - Brasil",
-               Gender = "Male",
-           };
-        }
-
+        //editar uma pessoa pelo id
         public Person Update(Person person)
         {
-            return person;
+            if (!Exists(person.Id))
+            {
+                return new Person();
+            }
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    
+                    throw;
+                }
+            }    
+           return person;
         }
 
+        //método chamado no create 
+        private bool Exists(long id)
+        {
+           return _context.Persons.Any(p => p.Id.Equals(id));
+        }
+
+        //deletar pelo id
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    
+                    throw;
+                }
+            }    
+        }
     }
 }
